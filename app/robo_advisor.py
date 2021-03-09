7af5bab5a38677507234004c0724e3be0acbc4d5
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-03-08 19:03:05 -0500

    Started the loop, did the datetime

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,11 +1,38 @@
 # this is the "app/robo_advisor.py" file
-# this is all https://github.com/prof-rossetti/intro-to-python/blob/master/projects/robo-advisor/README.md
+import os
+from datetime import datetime
+from dotenv import load_dotenv
 
+load_dotenv()
+now = datetime.now()
+
+
+
+while True:
+    symbol = input("Please input a stock or cryptocurency ticker symbol: ")
+    try:
+        float(symbol)
+        print("Oops, your symbol might includes a number(s). Please try again!")
+        continue
+    except ValueError:
+        pass
+
+    if len(symbol)<1 or len(symbol)>5
+        print("Oops, we need a stock between 1 to 5 characters. Please try again!")
+        continue
+    elif symbol.lower() in symbol:
+        print("Oops, looks like a duplicate. Please try again!")
+        continue
+    else:
+        symbol.append(symbol.lower())
+
+
+# this is mostly from https://github.com/prof-rossetti/intro-to-python/blob/master/projects/robo-advisor/README.md
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: ", now.strftime('%b %d, %Y  %I:%M %p'))
 print("-------------------------")
 print("LATEST DAY: 2018-02-20")
 print("LATEST CLOSE: $100,000.00")
